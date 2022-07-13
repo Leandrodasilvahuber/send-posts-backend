6993987217e309975397117d40f4fbe09ebfdaef
Leandro Hüber <emaildohuber@gmail.com>
2022-07-12 21:46:16 -0300

    Configurar db heroku

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import { JWTProvider } from './providers/implementations/JWTProvider'
-// import { createUserController } from './useCases/createUser/index'
-// import { loginController } from './useCases/login/index'
-// import { createPostController } from "./useCases/createPost/index"
+import { createUserController } from './useCases/createUser/index'
+import { loginController } from './useCases/login/index'
+// import { createPostController } from './useCases/createPost/index'
 
 const jwtProvider = new JWTProvider()
 
@@ -20,7 +20,6 @@ router.get('/posts', jwtProvider.verifyJWT, (request, response) => {
   response.send('<h1>Not Implemeted</h1>')
 })
 
-/*
 router.post('/login', (request, response, next) => {
   return loginController.handle(request, response)
 })
@@ -28,6 +27,5 @@ router.post('/login', (request, response, next) => {
 router.post('/users', (request, response) => {
   return createUserController.handle(request, response)
 })
-*/
 
 export { router }
